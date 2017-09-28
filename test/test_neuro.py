c13625bd71c3e7c197a051554a979ac9f5d5cc1e
Cristi Constantin <cristi.constantin@live.com>
2017-09-28 23:40:43 +0300

    Added another test for Neuro

--- test/test_neuro.py
+++ test/test_neuro.py
@@ -24,3 +24,56 @@ class TestLocal(unittest.TestCase):
             len(g.query_subject('means')), len(words))
         self.assertEqual(
             set(g.query_thing('means')), {'yes', 'no'})
+
+
+    def test_family(self):
+
+        g = Neuro()
+
+        mom = g.add_node('mom')
+        dad = g.add_node('dad')
+        girl = g.add_node('girl')
+        boy = g.add_node('boy')
+        cat = g.add_node('lazy cat')
+
+        # subject -> predicate -> thing
+        g.add_triple('mom', 'loves', 'dad')
+        g.add_triple('dad', 'loves', 'mom')
+        g.add_triple('mom', 'loves', 'girl')
+        g.add_triple('dad', 'loves', 'boy')
+        g.add_triple('mom', 'loves', 'lazy cat')
+
+        g.add_triple('girl', 'needs', 'mom')
+        g.add_triple('girl', 'needs', 'dad')
+        g.add_triple('boy', 'needs', 'mom')
+        g.add_triple('boy', 'needs', 'dad')
+        g.add_triple('lazy cat', 'needs', 'mom')
+
+        # Who needs something
+        self.assertListEqual(
+            sorted(g.query_subject('needs')), ['boy', 'girl', 'lazy cat'])
+        # Who is needed
+        self.assertListEqual(
+            sorted(g.query_thing('needs')), ['dad', 'mom'])
+
+        # Who loves someone
+        self.assertListEqual(
+            sorted(g.query_subject('loves')), ['dad', 'mom'])
+        # Who is loved by someone
+        self.assertListEqual(
+            sorted(g.query_thing('loves')), ['boy', 'dad', 'girl', 'lazy cat', 'mom'])
+
+        # Relations between mom & dad
+        self.assertListEqual(
+            g.query_triple('mom', '?', 'dad'), [('mom', 'loves', 'dad')])
+        self.assertListEqual(
+            g.query_triple('dad', '?', 'mom'), [('dad', 'loves', 'mom')])
+
+        # Who loves mom
+        self.assertListEqual(
+            g.query_triple('?', 'loves', 'mom'), [('dad', 'loves', 'mom')])
+        # Who needs mom
+        self.assertListEqual(
+            g.query_triple('?', 'needs', 'mom'),
+            [('girl', 'needs', 'mom'), ('boy', 'needs', 'mom'), ('lazy cat', 'needs', 'mom')]
+        )
