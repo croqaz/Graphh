ea67f61ee04d33db3cab36f19832bfa9c1526173
Cristi Constantin <cristi.constantin@live.com>
2017-10-04 15:06:56 +0300

    Added import export to CSV

---
+++ graphh/io/csv.py
@@ -0,0 +1,42 @@
+"""
+Import and export Graph to CSV.
+"""
+
+import time
+from binascii import hexlify, unhexlify
+
+
+def export_csv(graph, nodes_file='g_nodes.csv', edges_file='g_edges.csv'):
+    t1 = time.time()
+    with open(nodes_file, 'w') as nfile:
+        nfile.write('id,label\n')
+        for k, val in graph._nodes.items():
+            hex = hexlify(k).decode('utf')
+            nfile.write(f'{hex},{val}\n')
+
+    with open(edges_file, 'w') as efile:
+        efile.write('Source,Target\n')
+        for _, (head, tail) in graph._edges.items():
+            head = hexlify(head).decode('utf')
+            tail = hexlify(tail).decode('utf')
+            efile.write(f'{head},{tail}\n')
+    t2 = time.time()
+    print('Exported CSV in `{:.4f}` seconds.'.format(t2 - t1))
+
+
+def import_csv(graph, nodes_file='g_nodes.csv', edges_file='g_edges.csv'):
+    t1 = time.time()
+    with open(nodes_file, 'r') as nfile:
+        nfile.readline() # Ignore header
+        for line in nfile.readlines():
+            key, val = line[:-1].split(',')
+            graph.add_node(val) == unhexlify(key), 'The CSV nodes file is corrupted'
+
+    with open(edges_file, 'r') as efile:
+        efile.readline() # Ignore header
+        for line in efile.readlines():
+            head, tail = line[:-1].split(',')
+            graph.add_edge(unhexlify(head), unhexlify(tail))
+    t2 = time.time()
+    print('Imported CSV in `{:.4f}` seconds.'.format(t2 - t1))
+    return graph
--- test/test_io.py
+++ test/test_io.py
@@ -3,7 +3,7 @@ import os, sys
 sys.path.insert(1, os.getcwd())
 
 from graphh import Graph
-from graphh.io import cbor, msgpack
+from graphh.io import cbor, msgpack, csv
 from graphh.generators import generate_polygon_gr
 
 
@@ -28,9 +28,9 @@ def test_io_cbor():
     os.remove(pth)
 
 
-def test_io_json():
+def test_io_mpack():
     """
-    Import-Export JSON files
+    Import-Export MsgPack files
     """
     g = generate_polygon_gr(4)
 
@@ -47,6 +47,30 @@ def test_io_json():
     assert g.edge_list() == x.edge_list()
 
     os.remove(pth)
+
+
+def test_io_csv():
+    """
+    Import-Export CSV files
+    """
+    g = generate_polygon_gr(4)
+
+    n_pth = 'test/nodes.csv'
+    e_pth = 'test/edges.csv'
+    csv.export_csv(g, n_pth, e_pth)
+    assert os.path.isfile(n_pth)
+    assert os.path.isfile(e_pth)
+
+    x = Graph()
+    csv.import_csv(x, n_pth, e_pth)
+
+    assert g.number_of_nodes() == x.number_of_nodes()
+
+    assert g.node_list() == x.node_list()
+    assert g.edge_list() == x.edge_list()
+
+    os.remove(n_pth)
+    os.remove(e_pth)
 
 
 # The end
